6f3f47cfc6bd9a755f0dc85c59465b4ca2c62c20
Mehdi <mehdichaibi05@gmail.com>
2022-10-13 23:47:20 +0200

    add react access

---
+++ backend/settings.py
@@ -0,0 +1,10 @@
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+    "corsheaders",
+]
+
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
